88823ac3256d669c4d1a9b5b17a8682237676bb9
LeeJaeHyeok <dlwogurtkfkd@gmail.com>
2025-01-27 14:21:41 +0900

    init

--- src/components/common/Icon.tsx
+++ src/components/common/Icon.tsx
@@ -7,25 +7,19 @@ interface IconProps {
   opsz?: number;
 }
 
-// const StyledIcon = styled.i<PrefixWithDollar<IconProps, "iconName">>`
-//   user-select: none;
-//   font-family: "Material Symbols Rounded";
-//   font-size: inherit;
-//   display: inline-block;
-
-//   font-style: normal;
-
-//   transition: all 0.225s var(--ease-in-out);
-
-//   font-variation-settings:
-//     "FILL" ${props => props?.$fill ?? 0},
-//     "wght" ${props => props?.$wght ?? 400},
-//     "GRAD" ${props => props?.$grad ?? 0},
-//     "opsz" ${props => props?.$opsz ?? 24};
-// `;
-
 function Icon({ iconName, fill, grad, opsz, wght }: IconProps) {
-  return { iconName };
+  const style = {
+    fontVariationSettings: `
+    'FILL' ${fill ?? 0}, 
+    'wght' ${wght ?? 400}, 
+    'GRAD' ${grad ?? 0}, 
+    'opsz' ${opsz ?? 24}`,
+  };
+  return (
+    <i className="material-symbols-rounded transition-all" style={style}>
+      {iconName}
+    </i>
+  );
 }
 
 export default Icon;
--- src/domain/main/MainPage.tsx
+++ src/domain/main/MainPage.tsx
@@ -1,9 +1,14 @@
 import { Code } from "@heroui/react";
+import { Icon } from "@src/components/common";
+
 function MainPage() {
   return (
     <div>
       <h1>Main Page</h1>
-      <Code color="danger">Code Test</Code>
+      <div className="flex items-center text-red-500">
+        <Icon iconName="home" fill={1} />
+        <Code color="danger">Code Test</Code>
+      </div>
     </div>
   );
 }
